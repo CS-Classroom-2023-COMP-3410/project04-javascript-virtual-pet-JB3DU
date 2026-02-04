6e7571ce4548d0206da6501527fa36db6a9e1446
JB3DU <josh.barlow@du.edu>
2026-02-04 10:34:51 -0700

    Fixed small bug

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 let hunger = false;
 let sleepy = false;
 let bored = false;
+localStorage.setItem("hunger", "false");
+localStorage.setItem("sleepy", "false");
+localStorage.setItem("bored", "false");
 
 let intervalID = setInterval(changeNeed, 5000);
 
@@ -12,13 +15,15 @@ function changeNeed() {
     let need = Math.floor(Math.random() * 3);
     if(need === 0) {
         hunger = true;
-        bored = false;
+        localStorage.setItem("hunger", "true");
     } else if(need === 1) {
         sleepy = true;
         hunger=false;
+        localStorage.setItem("sleepy", "true");
     } else {
         bored = true;
         sleepy = false;
+        localStorage.setItem("bored", "true");
     }
     moodchange();
 }
@@ -52,17 +57,23 @@ document.getElementById("Buttons").addEventListener("click", function(event) {
     //does the opposite for test purposes
     if(event.target.id === "sleep") {
         sleepy = false;
-        petDisplay.innerHTML = '<img src="sleep.png">';
+        localStorage.setItem("sleepy", "false");
+        petDisplay.innerHTML = '<img src="pet_sleep.png">';
     } else if(event.target.id === "play") {
         bored = false;
+        localStorage.setItem("bored", "false");
         petDisplay.innerHTML = '<img src="pet_playing.png">';
     } else if(event.target.id === "feed") {
         hunger = false;
+        localStorage.setItem("hunger", "false");
         petDisplay.innerHTML = '<img src="pet_eating.png">';
     } else if(event.target.id === "evil") {
         hunger = true;
         sleepy = true;
         bored = true;
+        localStorage.setItem("hunger", "true");
+        localStorage.setItem("sleepy", "true");
+        localStorage.setItem("bored", "true");
     }
     let timeoutID = setTimeout(moodchange, 2500);
 });
